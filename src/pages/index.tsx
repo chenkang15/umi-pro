2dd529c00179e2dd6d31a271596b701c6104b6c9
zm <1476411577@qq.com>
2020-08-29 21:25:51 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styles from './index.less';
 export default () => {
-  console.log(123)
-  return (
-    <div>
-      test
-    </div>
-  );
-}
+    console.log(123);
+    console.log({aL: 1})
+    return <div>test</div>;
+};
