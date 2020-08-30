3be92d9c39dd7479c09997ea77a21825487c450f
zm <1476411577@qq.com>
2020-08-30 11:48:22 +0800

    feat: test

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         'prettier/prettier': 1,
         'no-console': ['warn', { allow: ['warn', 'error'] }],
         "eqeqeq": ['warn', 'always'],
-        "prefer-const": ['error', {"destructuring": "all", "ignoreReadBeforeAssign": true}],
+        "prefer-const": ['error', { "destructuring": "all", "ignoreReadBeforeAssign": true }],
         '@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
         '@typescript-eslint/no-unused-vars': 0,
         "@typescript-eslint/interface-name-prefix": 0,
@@ -40,6 +40,29 @@ module.exports = {
         // React相关校验规则
         "react/jsx-indent": [2, 4],
         "react/jsx-no-undef": [2, { allowGlobals: true }],
-        "jsx-control-statements/jsx-use-if-tag": 0
+        "jsx-control-statements/jsx-use-if-tag": 0,
+        'react/jsx-key': 2, //在数组或迭代器中验证JSX具有key属性
+        'react/jsx-max-props-per-line': [1, { maximum: 1 }], // 限制JSX中单行上的props的最大数量
+        'react/jsx-no-duplicate-props': 2, //防止在JSX中重复的props
+        'react/jsx-no-undef': 1, //在JSX中禁止未声明的变量
+        'react/no-string-refs': 1, //Using string literals in ref attributes is deprecated
+        'react/jsx-uses-react': 1, //防止反应被错误地标记为未使用
+        'react/jsx-uses-vars': 1, //防止在JSX中使用的变量被错误地标记为未使用
+        'react/no-danger': 1, //防止使用危险的JSX属性
+        'react/no-did-update-set-state': 2, //防止在componentDidUpdate中使用setState
+        'react/no-did-mount-set-state': 0, //防止在componentDidUpdate中使用setState
+        'react/no-direct-mutation-state': 2, //防止this.state赋值
+        'react/no-unknown-property': 2, //防止使用未知的DOM属性
+        'react/prefer-es6-class': 1, //为React组件强制执行ES5或ES6类
+        'react/react-in-jsx-scope': 0, //使用JSX时，必须要引入React
+        'react/sort-comp': 0, //强制组件方法顺序
+        'react/sort-prop-types': 0, //强制组件属性顺序
+        'react/jsx-sort-props': 1,
+        'react/no-deprecated': 1, //不使用弃用的方法
+        'react/jsx-equals-spacing': 1, //在JSX属性中强制或禁止等号周围的空格
+        'react/wrap-multilines': 0,
+        // react-hooks
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
     }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './index.less';
+const Test: React.FC<{ a: number }> = ({ a }): React.ReactNode => {
+    return <div>{a}</div>
+}
 const App = (): React.ReactNode => {
-    if (1) {}
-    return <div>test</div>;
+    if (1) { }
+    return <Test a={1} a={2}>test</Test>;
 };
 export default App;
